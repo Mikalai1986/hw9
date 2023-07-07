a0a8121704b918ad1f1ea0e2fb6cee9fe0542016
FostikN <fostik@tut.by>
2023-07-07 14:28:12 +0300

    решение задачи 68

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ PrintNumbers(num);
 Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30
-*/
+
 int InputNum(string message)
 {
     Console.Write(message);
@@ -36,3 +36,24 @@ int num2 = InputNum("Введите целое число N: ");
 
 int S = SumNumbers(num1, num2);
 Console.WriteLine($"сумма чисел = {S}");
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+int InputNum(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine()!);
+}
+int AccFunc(int M, int N)
+{
+    if (M == 0) return N + 1;
+    else if (N == 0) return AccFunc(M - 1, 1);
+    return AccFunc(M - 1, AccFunc(M, N - 1));
+
+}
+int num1 = InputNum("Введите целое положительное число m: ");
+int num2 = InputNum("Введите целое положительное число n: ");
+
+int S = AccFunc(num1, num2);
+Console.WriteLine($"A({num1}, {num2}) = {S}");
